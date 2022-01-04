dd82ec0a5313c2fa69012c928a68e805a3012bf5
Aleksey29247 <95174048+Aleksey29247@users.noreply.github.com>
2022-01-04 16:48:05 +0700

    Update BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,21 +1,9 @@
 public class BonusMilesService {
-   private int _bonus_miles;
-    public int get() {
-        return _bonus_miles;
-    }
-    public void set (int bonus_miles) {
-        _bonus_miles=bonus_miles;
-    }
-    public BonusMilesService(){
-
-        _bonus_miles=0;
-    };
-    public  int calculate(int cost)
-    {
-        if (cost< _bonus_miles)
+    public int calculate(int cost, int bonus_miles) {
+        if (cost < bonus_miles) {
             return 0;
-        else
-            return (int)(cost/_bonus_miles);
+        } else {
+            return (int) (cost / bonus_miles);
+        }
     }
-
 }
